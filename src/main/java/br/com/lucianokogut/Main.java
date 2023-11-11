b0b868206024c9a19ab76af27272d673ecdf0c88
Luciano Kogut <ljkogut@hotmail.com>
2023-11-11 15:20:49 -0300

    feat: implementado os objetos para primeira inclusão

--- src/main/java/br/com/lucianokogut/Main.java
+++ src/main/java/br/com/lucianokogut/Main.java
@@ -1,7 +1,48 @@
-package br.com.lucianokogut;
+/* package br.com.lucianokogut;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+}
+*/
+package br.com.lucianokogut;
+
+import br.com.lucianokogut.model.bo.PessoaBO;
+import br.com.lucianokogut.model.vo.PessoaVO;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        PessoaBO pessoaBO = new PessoaBO();
+
+        //Cadastrar Pessoa (cp)
+
+        //Cadastro OK
+        PessoaVO cp1 = new PessoaVO(0, "Adriano", "01234567899", 45);
+        System.out.println(cp1.getIdPessoa() >0 ? "Pessoa Cadastrada com Sucesso!!!" : "Houve um problema no Cadastro!!");
+
+        //Cadastro OK
+        PessoaVO cp2 = new PessoaVO(0, "Maria", "01234567898", 20);
+        System.out.println(cp2.getIdPessoa() >0 ? "Pessoa Cadastrada com Sucesso!!!" : "Houve um problema no Cadastro!!");
+
+        //Cadastro OK
+        PessoaVO cp3 = new PessoaVO(0, "Luciano", "01234567897", 40);
+        System.out.println(cp3.getIdPessoa() >0 ? "Pessoa Cadastrada com Sucesso!!!" : "Houve um problema no Cadastro!!");
+
+        //Menor de Idade
+        PessoaVO cp4 = new PessoaVO(0, "José", "01234567896", 15);
+        System.out.println(cp4.getIdPessoa() >0 ? "Pessoa Cadastrada com Sucesso!!!" : "Houve um problema no Cadastro!!");
+
+        //CPF nulo (validado por uma procedure que roda no BD)
+        PessoaVO cp5 = new PessoaVO(0, "João", null, 25);
+        System.out.println(cp5.getIdPessoa() >0 ? "Pessoa Cadastrada com Sucesso!!!" : "Houve um problema no Cadastro!!");
+
+        //Tentando cadastrar uma pessoa já cadastrada
+        cp1 = pessoaBO.cadastrarPessoaBO(cp1);
+        System.out.println(cp1.getIdPessoa() >0 ? "Pessoa Cadastrada com Sucesso!!!" : "Houve um problema no Cadastro!!");
+
+        System.out.println("\n-----------------------------------\n");
     }
 }
