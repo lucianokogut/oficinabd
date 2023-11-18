9ffb7fd5f354a6f2aa3e4f317b004d726dd0e3c2
Luciano Kogut <ljkogut@hotmail.com>
2023-11-18 16:16:54 -0300

    feat: implementado classe com função de Controller para permitir o acesso via endpoints do backend puro

---
+++ src/main/java/br/com/lucianokogut/controller/PessoaRest.java
@@ -0,0 +1,67 @@
+package br.com.lucianokogut.controller;
+
+import java.util.List;
+
+import br.com.lucianokogut.model.bo.PessoaBO;
+import br.com.lucianokogut.model.vo.PessoaVO;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+//import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("/pessoa")
+public class PessoaRest {
+	
+	@POST
+	@Path("/cadastrar")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public PessoaVO cadastrarPessoaController(PessoaVO pessoaVO) {
+		PessoaBO pessoaBO = new PessoaBO();
+		return pessoaBO.cadastrarPessoaBO(pessoaVO);
+	}
+	
+	@GET
+	@Path("/listar")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<PessoaVO> consultarTodasPessoasController() {
+		PessoaBO pessoaBO = new PessoaBO();
+		return pessoaBO.consultarTodasPessoasBO();
+	}
+	
+/*
+ 	@GET
+	@Path("/pesquisar/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public PessoaVO consultarPessoaController(@PathParam("id") int id) {
+		PessoaBO pessoaBO = new PessoaBO();
+		return pessoaBO.consultarPessoaBO(id);
+	}
+*/
+
+	@PUT
+	@Path("/atualizar")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Boolean atualizarPessoaController(PessoaVO pessoaVO) {
+		PessoaBO pessoaBO = new PessoaBO();
+		return pessoaBO.atualizarPessoaBO(pessoaVO);
+	}
+	
+	@DELETE
+	@Path("/excluir")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Boolean excluirPessoaController(PessoaVO pessoaVO) {
+		PessoaBO pessoaBO = new PessoaBO();
+		return pessoaBO.excluirPessoaBO(pessoaVO);
+	}
+	
+}
